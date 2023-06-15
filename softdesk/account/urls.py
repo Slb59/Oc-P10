39b34b96f61e4b112f7504a719dc72e4bac7c60a
Slb59 <sylvie.bricout2@laposte.net>
2023-06-15 15:43:56 +0200

    add account_router

--- softdesk/account/urls.py
+++ softdesk/account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from rest_framework import routers
 
 app_name = "account"
+
+account_router = routers.SimpleRouter()
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
