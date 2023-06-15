b9e8f0c4c7622329ef4f6dadb75127f5f07af152
Slb59 <sylvie.bricout2@laposte.net>
2023-06-15 15:56:20 +0200

    add helpdesk router

--- softdesk/account/urls.py
+++ softdesk/account/urls.py
@@ -4,6 +4,8 @@ from rest_framework import routers
 app_name = "account"
 
 account_router = routers.SimpleRouter()
+# account_router.register('login', ?? )
+# account_router.register('signup', ?? )
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
--- softdesk/helpdesk/urls.py
+++ softdesk/helpdesk/urls.py
@@ -1,4 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
 app_name = "helpdesk"
 
+helpdesk_router = routers.SimpleRouter()
+# helpdesk_router.register('projects', ?? , basename='projects')
+
 urlpatterns = [
+    path('', include(helpdesk_router.urls))
 ]
