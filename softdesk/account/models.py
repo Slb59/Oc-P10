5c39dab914027ace73284ef8a57cda5df2fbd7b9
Slb59 <sylvie.bricout2@laposte.net>
2023-06-19 16:04:50 +0200

    add admin registers

--- softdesk/account/models.py
+++ softdesk/account/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth import models as auth_models
 
 class User(auth_models.AbstractUser):
     user_id = models.AutoField(primary_key=True)
+    post_description = models.CharField(max_length=128, default="")
--- softdesk/helpdesk/admin.py
+++ softdesk/helpdesk/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Issue, Comment
+from .models import Contributor
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "type"]
+    list_filter = ["type"]
+    search_fields = ["title", "description"]
+    raw_id_fields = ["author"]
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ["title", "tag", "priority", "status", "created_time"]
+    list_filter = ["tag", "priority", "status"]
+    search_fields = ["title", "description"]
+    ordering = ["created_time"]
+    raw_id_fields = ["author", "assignee"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["description", "created_time", "issue", "author"]
+    search_fields = ["description", "author__username"]
+    ordering = ["created_time"]
+    raw_id_fields = ["author"]
+
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = [
+        "project_contributor", "user_contributor", "role", "permission"
+        ]
+    list_filter = ["role", "permission"]
+    search_fields = [
+        "project_contributor__title", "user_contributor__username"
+        ]
+    ordering = ["project_contributor"]
+    raw_id_fields = ["project_contributor", "user_contributor"]
