c0e87743578b352942ddee39a40abe84f5e7bacc
Slb59 <sylvie.bricout2@laposte.net>
2023-06-19 17:35:29 +0200

    custom admin user

--- softdesk/account/admin.py
+++ softdesk/account/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
+# from .forms import CustomUserCreationForm, CustomUserChangeForm
 User = get_user_model()
 
-admin.site.register(User, auth_admin.UserAdmin)
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    list_display = ['username', 'date_joined', 'is_staff', 'post_description']
+    fieldsets = auth_admin.UserAdmin.fieldsets + (
+        (None, {"fields": ["post_description"]}),)
+    add_fieldsets = auth_admin.UserAdmin.add_fieldsets + (
+        (None, {"fields": ["post_description"]}),)
--- softdesk/account/models.py
+++ softdesk/account/models.py
@@ -6,3 +6,6 @@ from django.contrib.auth import models as auth_models
 class User(auth_models.AbstractUser):
     user_id = models.AutoField(primary_key=True)
     post_description = models.CharField(max_length=128, default="")
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
