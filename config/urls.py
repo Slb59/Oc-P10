4a4443562bb6a8ba95e62db6385a361ad87ff228
Slb59 <sylvie.bricout2@laposte.net>
2023-06-21 15:45:02 +0200

    add docs urls

--- config/urls.py
+++ config/urls.py
@@ -4,13 +4,21 @@ URL configuration for softdesk project.
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
 
-schema_view = get_schema_view(title='SoftDesk API')
+API_TITLE = 'SoftDesk API'
+API_DESCRIPTION = 'A Web API for tracking issue on projects'
+
+schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("softdesk.account.urls")),
-    path("", include("softdesk.helpdesk.urls")),
+    path("", include("softdesk.account.urls")),
+    path("projects/", include("softdesk.helpdesk.urls")),
     path('schema/', schema_view),
+    path('docs/', include_docs_urls(
+        title=API_TITLE,
+        description=API_DESCRIPTION
+        )),
 ]
