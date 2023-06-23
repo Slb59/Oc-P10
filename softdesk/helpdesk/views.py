93e12f71608b9988f259f1c65e7e2ec7c769157f
Slb59 <sylvie.bricout2@laposte.net>
2023-06-23 14:31:00 +0200

    add project viewset

--- softdesk/helpdesk/views.py
+++ softdesk/helpdesk/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+    """
+    API endpoints to create, view, edit or delete a project
+    """
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(contributors=self.request.user)
