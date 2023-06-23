a6715862a1d68352b8682f67f2fb7608ccc8ea42
Slb59 <sylvie.bricout2@laposte.net>
2023-06-23 14:33:53 +0200

    add project url

--- softdesk/helpdesk/urls.py
+++ softdesk/helpdesk/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
+
+from .views import ProjectViewSet
 
 app_name = "helpdesk"
 
 helpdesk_router = routers.SimpleRouter()
-# helpdesk_router.register('projects', ?? , basename='projects')
+helpdesk_router.register('projects', ProjectViewSet, basename='projects')
 
 urlpatterns = [
     path('', include(helpdesk_router.urls))
